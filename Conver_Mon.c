e59819088f2bd74a7086969f96b6cd300b7159b0
Azet13 <159089767+Azet13@users.noreply.github.com>
2024-03-11 22:55:22 -0600

    Update Conver_Mon.c

--- Conver_Mon.c
+++ Conver_Mon.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 
-#define euro 1
-#define dolar 1.26
+#define dolar 7.75
 
 int main () {
    
-    float vdolar, veuro, v_e, v_d;
-    veuro=0;
+    float vdolar, vquet, v_d;
+    vquet=0;
     vdolar=0;
     v_d=0;
-    v_e=0;
    
-        printf ("Introduzca la primera cantidad en euros: \n");
-        scanf ("%f", &veuro);
-            v_d=veuro*dolar;
+        printf ("Introduzca la primera cantidad en dolares: \n");
+        scanf ("%f", &vquet);
+            v_d=vquet*dolar;
             printf ("%.2f \n", v_d);
-            printf ("En dólares son %.2f \n", v_d);
-       
-        printf ("Introduzca la segunda cantidad en dólares: \n");
-        scanf ("%f", &vdolar);
-            v_e=euro*vdolar/dolar;
-            printf ("%.2f \n", v_e);   
-            printf ("En euros son son %.2f \n", v_e);
+            printf ("En Quetzales son %.2f \n", v_d);
+      
 }
